c3fab305d4fdb1217cf2a6e9eeab936167bf8733
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2022-12-07 21:55:31 +1300

    Update fxmanifest.lua

--- VespucciFestival/fxmanifest.lua
+++ VespucciFestival/fxmanifest.lua
@@ -1,65 +1,17 @@
 fx_version 'cerulean'
 game 'gta5'
 
+
+
+authors { 'TayMcKenzieNZ' }
+description 'Vespucci Festival'
+version '1.0.0'
+ url 'https://github.com/TayMcKenzieNZ/Vespucci-Festival'
+
 this_is_a_map 'yes'
 
-data_file "DLC_ITYP_REQUEST" "laptop_tay.ytyp"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-server_script '/stream/**/*.js'
--- RARE5M.CO
+---                                                                           ---
+---   THIS IS A FREE RESOURCE PROVIDED & UPLOADED TO GITHUB FOR FIVEM USAGE   ---
+---        DO NOT REUPLOAD OR MODIFY WITHOUT MY PERMISSION                    ---
